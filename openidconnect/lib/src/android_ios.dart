269d95ea459108d444d84dd6b2829042fe397858
besteFinanzen <niklas@kimmerles.de>
2023-08-15 19:30:32 +0200

    Added Progress Indicator

--- openidconnect/lib/src/android_ios.dart
+++ openidconnect/lib/src/android_ios.dart
@@ -21,7 +21,7 @@ class OpenIdConnectAndroidiOS {
     } else {
       params = const flutterWebView.PlatformWebViewControllerCreationParams();
     }
-
+    int progress = 0;
     final flutterWebView.WebViewController _webviewcontroller =
         flutterWebView.WebViewController.fromPlatformCreationParams(params)
           ..setJavaScriptMode(flutterWebView.JavaScriptMode.unrestricted)
@@ -41,78 +41,97 @@ class OpenIdConnectAndroidiOS {
       context: context,
       barrierDismissible: false,
       builder: (dialogContext) {
-        _webviewcontroller
-            .setNavigationDelegate(flutterWebView.NavigationDelegate(
-          onNavigationRequest: (navigation) async {
-            if (navigation.url.startsWith(redirectUrl)) {
-              if (dialogContext.mounted) {
-                Navigator.pop(dialogContext, navigation.url);
-              }
-              return flutterWebView.NavigationDecision.navigate;
-            }
-            if (navigationInterceptor != null) {
-              var interceptionResult =
+        return StatefulBuilder(
+          builder: (context, setState) {
+            _webviewcontroller
+                .setNavigationDelegate(flutterWebView.NavigationDelegate(
+              onProgress: (int _progress) {
+                setState(() {
+                  progress = _progress;
+                });
+              },
+              onNavigationRequest: (navigation) async {
+                if (navigation.url.startsWith(redirectUrl)) {
+                  if (dialogContext.mounted) {
+                    Navigator.pop(dialogContext, navigation.url);
+                  }
+                  return flutterWebView.NavigationDecision.navigate;
+                }
+                if (navigationInterceptor != null) {
+                  var interceptionResult =
                   await navigationInterceptor.call(context, navigation);
 
-              if (interceptionResult != null) return interceptionResult;
-            }
-            return flutterWebView.NavigationDecision.navigate;
-          },
-        ));
-        return Visibility(
-          visible: !inBackground,
-          child: AlertDialog(
-            elevation: 0,
-            shadowColor: Colors.transparent,
-            backgroundColor: Colors.transparent,
-            insetPadding: EdgeInsets.zero,
-            titlePadding: EdgeInsets.zero,
-            contentPadding: EdgeInsets.zero,
-            actionsPadding: EdgeInsets.zero,
-            content: WillPopScope(
-              // Catched back button pressed
-              onWillPop: () async {
-                if (await _webviewcontroller.canGoBack()) {
-                  await _webviewcontroller.goBack();
-                  return false;
+                  if (interceptionResult != null) return interceptionResult;
                 }
-                return true;
+                return flutterWebView.NavigationDecision.navigate;
               },
-              child: Stack(
-                children: [
-                  Container(
-                    width: min(popupWidth, MediaQuery.of(context).size.width),
-                    height: min(popupHeight, MediaQuery.of(context).size.height),
-                    child: GestureDetector(
-                      onHorizontalDragUpdate: (_) {},
-                      child: flutterWebView.WebViewWidget(
-                        controller: _webviewcontroller,
+            ));
+            return Visibility(
+              visible: !inBackground,
+              child: AlertDialog(
+                elevation: 0,
+                shadowColor: Colors.transparent,
+                backgroundColor: Colors.transparent,
+                insetPadding: EdgeInsets.zero,
+                titlePadding: EdgeInsets.zero,
+                contentPadding: EdgeInsets.zero,
+                actionsPadding: EdgeInsets.zero,
+                content: WillPopScope(
+                  // Catched back button pressed
+                  onWillPop: () async {
+                    if (await _webviewcontroller.canGoBack()) {
+                      await _webviewcontroller.goBack();
+                      return false;
+                    }
+                    return true;
+                  },
+                  child: Stack(
+                    children: [
+                      Container(
+                        width: min(popupWidth, MediaQuery.of(context).size.width),
+                        child: LinearProgressIndicator(
+                          value: progress / 100,
+                          backgroundColor: Colors.transparent,
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                              backgroundColor ?? Colors.blue),
+                        ),
                       ),
-                    ),
+                      Container(
+                        width: min(popupWidth, MediaQuery.of(context).size.width),
+                        height: min(popupHeight, MediaQuery.of(context).size.height),
+                        child: GestureDetector(
+                          onHorizontalDragUpdate: (_) {},
+                          child: flutterWebView.WebViewWidget(
+                            controller: _webviewcontroller,
+
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 0,
+                        left: 0,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white24,
+                              borderRadius: BorderRadius.only(
+                                  bottomRight: Radius.circular(20))),
+                          child: IconButton(
+                            key: const Key(
+                                'openidconnect__close_auth_window_button'),
+                            onPressed: () {
+                              if (dialogContext.mounted)
+                                Navigator.pop(dialogContext, null);
+                            },
+                            icon: Icon(Icons.close),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
-                  Positioned(
-                    top: 0,
-                    left: 0,
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: Colors.white24,
-                          borderRadius: BorderRadius.only(
-                              bottomRight: Radius.circular(20))),
-                      child: IconButton(
-                        key: const Key(
-                            'openidconnect__close_auth_window_button'),
-                        onPressed: () {
-                          if (dialogContext.mounted)
-                            Navigator.pop(dialogContext, null);
-                        },
-                        icon: Icon(Icons.close),
-                      ),
-                    ),
-                  )
-                ],
+                ),
               ),
-            ),
-          ),
+            );
+          }
         );
       },
     );
